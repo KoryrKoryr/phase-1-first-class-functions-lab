05bf9cc996f98b4cf54e1574377107bf8ba65e31
Denis Korir <denohrkoryr@gmail.com>
2024-07-08 14:01:08 +0300

    Functions Lab 2

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+  if (Array.isArray(drivers) && drivers.length >= 2) {
+    return drivers.slice(0, 2);
+  } else {
+    return [];
+  }
+};
+
+const returnLastTwoDrivers = function (drivers) {
+  if (Array.isArray(drivers) && drivers.length >= 2) {
+    return drivers.slice(-2);
+  } else {
+    return [];
+  }
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, selector) {
+  return selector(drivers);
+};
